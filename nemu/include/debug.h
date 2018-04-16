eabe9cbc5e1db63cc9efbd453f9f82b41d043267
tracer-ics2017 <tracer@njuics.org>
2018-04-16 16:31:08 +0000

    >  compile 161220000 ics Linux 4d40a42cd8f9 4.9.87-linuxkit-aufs #1 SMP Wed Mar 14 15:12:16 UTC 2018 x86_64 GNU/Linux  16:31:08 up 44 min,  2 users,  load average: 0.10, 0.06, 0.01 1f3901e1045d11bcbe5ae8fe386d59a17585618

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -19,7 +19,7 @@ extern FILE* log_fp;
 
 #define Log(format, ...) \
   do { \
-    fprintf(stdout, "\33[1;34m[%s,%d,%s] " format "\33[0m\n", \
+    fprintf(stdout, "LOG: \33[1;34m[%s,%d,%s] " format "\33[0m\n", \
         __FILE__, __LINE__, __func__, ## __VA_ARGS__); \
     fflush(stdout); \
     Log_write("[%s,%d,%s] " format "\n", \
@@ -30,7 +30,7 @@ extern FILE* log_fp;
   do { \
     if (!(cond)) { \
       fflush(stdout); \
-      fprintf(stderr, "\33[1;31m"); \
+      fprintf(stderr, "ASSERT: \33[1;31m"); \
       fprintf(stderr, __VA_ARGS__); \
       fprintf(stderr, "\33[0m\n"); \
       assert(cond); \
