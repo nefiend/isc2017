0b4459eb5231af0e847fb4739b481bfaed4cd3ed
tracer-ics2017 <tracer@njuics.org>
2018-04-19 17:13:59 +0000

    >  compile 161220000 ics Linux c993c8a57129 4.9.87-linuxkit-aufs #1 SMP Wed Mar 14 15:12:16 UTC 2018 x86_64 GNU/Linux  17:13:59 up  2:39,  3 users,  load average: 0.02, 0.01, 0.00 72de07c7ad4a4737d0ae03a297f06d45615d5ff7

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -50,12 +50,18 @@ static int cmd_info(char *args){
   if(NULL == args){
     printf("Please type r or w for params!\n");}
   else if(strcmp(args, "r") == 0){
-    /*char *reg[] = {"rax", "rcx", "rdx", "rbx", "rsp", "rbp", "rsi", "rdi", "rip"};
-    for (uint32_t i = 0; i < 9; i++){
-      uint32_t *temp = (uint32_t*)(&cpu+ 4 * i);
-      printf("%s \t %p \t 0x%x\n", reg[i], temp, *temp);
-    }*/
-    printf("eip = %x\n", cpu.eip);
+    char *reg[] = {"rax", "rcx", "rdx", "rbx", "rsp", "rbp", "rsi", "rdi", "rip"};
+    for (uint32_t i = 0; i < 8; i++){
+      printf("%s \t 0x%x\n",  reg[i], reg_l(i));
+      printf("%s \t 0x%x\n",  reg[i], reg_w(i));
+      printf("%s \t 0x%x\n",  reg[i], reg_b(i));
+
+      
+      
+      //uint32_t *temp = (uint32_t*)(&cpu + 4 * i);
+      //printf("%s \t %p \t 0x%x\n", reg[i], temp, *temp);
+    }
+    printf("rip \t 0x%x\n", cpu.eip);
   }
   else if(strcmp(args, "w") == 0){
 
