2baee5dffa8872936b12ac831773ec42874d746a
tracer-ics2017 <tracer@njuics.org>
2018-04-20 16:52:14 +0000

    >  compile 161220000 ics Linux c993c8a57129 4.9.87-linuxkit-aufs #1 SMP Wed Mar 14 15:12:16 UTC 2018 x86_64 GNU/Linux  16:52:14 up  4:19,  3 users,  load average: 0.00, 0.03, 0.00 98c7cc27637004bfd87b2fe0b0d27c42306dec0

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -145,7 +145,7 @@ uint32_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-
+  Log("expr ok!");
   /* TODO: Insert codes to evaluate the expression. */
   *success = true;
   TODO();
