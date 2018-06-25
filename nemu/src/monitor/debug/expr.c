054a11c3922ef4d0c18a41a52c9801dc3db2d99b
nefiend <nefiend1992@gmail.com>
2018-06-25 14:53:30 +0000

    >  compile 14202 ics Linux c993c8a57129 4.9.87-linuxkit-aufs #1 SMP Wed Mar 14 15:12:16 UTC 2018 x86_64 GNU/Linux  14:53:30 up  1:20,  2 users,  load average: 0.00, 0.00, 0.00 b8c2e059b1a07bf8be99655a35b98e358777e11d

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -51,7 +51,7 @@ static struct rule {
   {"0x[0-9a-fA-F]+", TK_NUM_HEX}, // hex number 
   {"[0-9]+", TK_NUM},     // number
   {"[a-zA-Z]+", TK_VAR},  // variable
-  {"$[a-zA-Z]", TK_REG_NAME},     // reg name
+  {"$[a-zA-Z]+", TK_REG_NAME},     // reg name
   {",", ','},             // comma
   {"[.(.]", '('},         // open parenthesis
   {"[.).]", ')'}         // close parenthesis
